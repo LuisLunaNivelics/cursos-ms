78ea9fac0d59f1684a2b752b2d9a06a9a261def0
Luis Luna <luiscarloslunach@gmail.com>
2021-11-18 14:30:54 -0500

    se crea la entidad y aplication.yml

---
+++ src/main/java/com/nivelics/cursosms/CursosMsApplication.java
@@ -0,0 +1,13 @@
+package com.nivelics.estudiantesms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.nivelics.estudiantesms.CursosMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nivelics/cursosms/CursosMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nivelics.estudiantesms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
